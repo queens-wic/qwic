bf2be5cde8bc951679b3dd19b7f731858fdb1ccd
reneehjk <reneehjkim11@gmail.com>
2024-06-26 20:36:53 -0400

    header w/o links

--- app/HeaderFooter/components/Header.js
+++ app/HeaderFooter/components/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Image from 'next/image';
 import './Header.css';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo_1.png';
 
 const Header = () => {
   return (
     <div className="header">
-      <Image src={logo} alt="Logo" width={50} height={50} className="header-image" />
-      <div className="header-text">Who Are We?</div>
-      <div className="header-text">Events</div>
-      <div className="header-text">Resources For Students</div>
-      <div className="header-text">Partnerships</div>
-      <div className="header-text">Our Team</div>
-      <div className="header-text">Contact Us</div>
+      <div className="header-image-container">
+        <Image src={logo} alt="Logo" className="header-image" />
+      </div>
+      <div className="header-text-container">
+        <div className="header-text">Who Are We</div>
+        <div className="header-text">Events</div>
+        <div className="header-text">Resources For Students</div>
+        <div className="header-text">Partnerships</div>
+        <div className="header-text">Our Team</div>
+      </div>
+      <div className="contact-us-container">
+        <div className="header-text">Contact Us</div>
+      </div>
     </div>
   );
 }
