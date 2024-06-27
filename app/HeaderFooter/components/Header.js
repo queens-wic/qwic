47a3564254faf142ad47039da9a593d83a9d60bf
reneehjk <reneehjkim11@gmail.com>
2024-06-26 20:43:33 -0400

    updated with tailwind.css

--- app/HeaderFooter/components/Header.js
+++ app/HeaderFooter/components/Header.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
-import './Header.css';
 import logo from '../assets/logo_1.png';
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="header-image-container">
-        <Image src={logo} alt="Logo" className="header-image" />
+    <div className="relative flex font-bold text-[#202632] bg-transparent pr-20">
+      <div className="relative top-[74px] w-[122px] h-[37px] mr-[375px] left-[74px]">
+        <Image src={logo} alt="Logo" className="object-contain w-full h-full" />
       </div>
-      <div className="header-text-container">
-        <div className="header-text">Who Are We</div>
-        <div className="header-text">Events</div>
-        <div className="header-text">Resources For Students</div>
-        <div className="header-text">Partnerships</div>
-        <div className="header-text">Our Team</div>
+      <div className="relative top-[74px] flex gap-[120px] items-center">
+        <div className="font-HeaderFont text-[15px]">Who Are We</div>
+        <div className="font-HeaderFont text-[15px]">Events</div>
+        <div className="font-HeaderFont text-[15px]">Resources For Students</div>
+        <div className="font-HeaderFont text-[15px]">Partnerships</div>
+        <div className="font-HeaderFont text-[15px]">Our Team</div>
       </div>
-      <div className="contact-us-container">
-        <div className="header-text">Contact Us</div>
+      <div className="relative top-[74px] flex items-center left-[270px]">
+        <div className="font-HeaderFont text-[15px]">Contact Us</div>
       </div>
     </div>
   );
