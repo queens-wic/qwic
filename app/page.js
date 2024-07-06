cbfe19da0ea7a44c2540ce375302684961b08ddf
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2024-07-06 19:16:30 -0400

    card-text accepting props

--- app/page.js
+++ app/page.js
@@ -8,7 +8,12 @@ export default function Home() {
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>QWIC Website</h1>
-        <CardText bcolor="dark-pink" fcolor="light-pink"/>
+        <CardText 
+          bcolor="dark-pink" 
+          fcolor="light-pink" 
+          title="Title" 
+          body="Fostering community for all women, underrepresented minorities, and anyone passionate about technology."
+        />
         <CardImg/>
       </main>
     </div>
