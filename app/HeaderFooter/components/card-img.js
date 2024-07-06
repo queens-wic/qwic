5775237acc017e361d411098e5d07594acc17c71
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2024-07-06 18:57:27 -0400

    card components hard coded

--- app/HeaderFooter/components/card-img.js
+++ app/HeaderFooter/components/card-img.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import Image from 'next/image';
-import image from '../assets/image_7.png';
+import image from '../assets/img_7.png';
 
 const CardImg = () => {
     // const bgClass = classNames({
@@ -10,11 +10,11 @@ const CardImg = () => {
     // });
 
     return (
-        <div className="relative w-[200px] h-[300px]">
-            <div className="absolute top-0 left-5 z-40 w-[250px] h-[300px] rounded-2xl border-1 border-black bg-light-pink">
-                <Image src={image} alt="Logo" className="object-contain w-full h-full" />
+        <div className="relative w-[200px] h-[200px]">
+            <div className="absolute top-0 left-5 z-40 w-[250px] h-[250px] rounded-2xl border-1 border-black bg-light-pink">
+                <Image src={image} alt="img" className="object-contain w-full h-full" />
             </div>
-            <div className="absolute top-5 left-0 z-30 w-[250px] h-[300px] rounded-2xl bg-dark-pink"></div>
+            <div className="absolute top-5 left-0 z-30 w-[250px] h-[250px] rounded-2xl bg-dark-pink"></div>
         </div>
     );
 };
