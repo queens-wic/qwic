12c00ca538b3a695e7278ac00a97ddee4e989ba9
kale <18kmkf@queensu.ca>
2024-07-07 16:39:55 -0400

    Initialize routes

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Header from "./HeaderFooter/components/Header";
-
+import Hero from "@/components/landing-page/Hero";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>QWIC Website</h1>
+        <Hero />
       </main>
     </div>
   );
---
+++ components/landing-page/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1 className="text-7xl font-semibold">
+        Helping you discover <br /> technology and feel empowered
+      </h1>
+      <p className="text-lg">
+        Fostering community for all women, underrepresented minorities, and
+        anyone passionate about technology.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
