d0110c328cc732abb119759031e227b2358489fb
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2024-07-07 22:17:46 -0400

    alt route

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
-import React from "react";
-// import Header from "./HeaderFooter/components/Header";
-import Hero from "@/components/landing-page/Hero"; // eventually I (kale) will add an index.js to clean up these imports
-import WhoAreWe from "@/components/landing-page/WhoAreWe";
-import OurTeam from "@/components/landing-page/OurTeam";
-import Hackathon from "@/components/landing-page/Hackathon";
-import SupportUs from "@/components/landing-page/SupportUs";
+import React from 'react';
+import Header from "./HeaderFooter/components/Header";
 
 export default function Home() {
   return (
