4f6f2c4ae79c6e4081266f36d308a3356cdb6534
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2024-07-07 21:08:10 -0400

    testimonia card done -no w h

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Header from "./HeaderFooter/components/Header";
 import CardImg from "./HeaderFooter/components/card-img";
 import CardText from "./HeaderFooter/components/card-text";
 import CardShadow from "./HeaderFooter/components/card-shadow";
+import Testimonial from './HeaderFooter/components/card-testimonial';
 import Icon from '/Users/kavitathomas/Documents/qwic_dev/qwic/app/HeaderFooter/assets/Vector.png'; //replace this, works for testing purposes
 import pic from '/Users/kavitathomas/Documents/qwic_dev/qwic/app/HeaderFooter/assets/img_7.png';
 
@@ -36,6 +37,14 @@ export default function Home() {
           title="Apply to be a mentee"
           body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
           buttontext="Become a Mentee"
+        />
+
+        <div className="p-10"/>
+
+        <Testimonial
+        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
+        name="Kavita Thomas"
+        info="4th year Computing Student"
         />
 
       </main>
