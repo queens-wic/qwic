d26c6d04b51ab553c639045723c6d6e352f4d955
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2024-07-08 20:16:09 -0400

    6 color cards, 1 shadow card complete

--- app/components/card-text-blue.js
+++ app/components/card-text-blue.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Image from 'next/image';
 import arrow from '../assets/arrow.png';
 
-const CardText = ({bcolor, fcolor, title, body, img, more}) => 
+const CardTextBlue = ({title, body, img, more}) => 
 
     (
       
       <div className="relative w-[250px] h-[350px]">
 
-        <div className={`absolute top-0 left-5 z-40 w-[225px] h-[275px] rounded-2xl border-1 border-black ${fcolor ? `bg-${fcolor}` : ''}`}>
+        <div className="absolute top-0 left-5 z-40 w-[225px] h-[275px] rounded-2xl border-1 border-black bg-light-blue">
           {img && <Image src={img} alt="Card Image" className="m-6 mt-8 mb-4 w-12 h-auto" />}
           <h2 className="m-6 mb-2 text-xl font-medium">{title}</h2>
           <p className="m-6 mt-2 text-xs">{body}</p>
@@ -18,10 +18,10 @@ const CardText = ({bcolor, fcolor, title, body, img, more}) =>
           </div>
         </div>
 
-        <div className={`absolute top-5 left-0 z-30 w-[225px] h-[275px] rounded-2xl ${bcolor ? `bg-${bcolor}` : ''}`}></div>
+        <div className="absolute top-5 left-0 z-30 w-[225px] h-[275px] rounded-2xl bg-dark-blue"></div>
 
       </div>
     
     );
 
-export default CardText;
+export default CardTextBlue;
--- app/page.js
+++ app/page.js
@@ -1,53 +1,7 @@
 import React from 'react';
-import Header from "./HeaderFooter/components/Header";
-import CardImg from "./HeaderFooter/components/card-img";
-import CardText from "./HeaderFooter/components/card-text-alt";
-import CardShadow from "./HeaderFooter/components/card-shadow";
-import Testimonial from './HeaderFooter/components/card-testimonial';
-import Icon from '/Users/kavitathomas/Documents/qwic_dev/qwic/app/HeaderFooter/assets/Vector.png'; //replace this, works for testing purposes
-import pic from '/Users/kavitathomas/Documents/qwic_dev/qwic/app/HeaderFooter/assets/img_7.png';
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
         <h1>QWIC Website</h1>
-
-        <CardText 
-          bcolor="dark-pink" 
-          fcolor="light-pink" 
-          title="Community" 
-          body="A space for all women, underrepresented minorities, and anyone passionate about technology."
-          img={Icon}
-          more="Learn more"
-        />
-
-        <div className="p-10"/>
-
-        <CardImg
-          bcolor="dark-pink"
-          img={pic}
-        />
-
-        <div className="p-10"/>
-
-        <CardShadow
-          title="Apply to be a mentee"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
-          buttontext="Become a Mentee"
-        />
-
-        <div className="p-10"/>
-
-        <Testimonial
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
-        name="Kavita Thomas"
-        info="4th year Computing Student"
-        />
-
-      </main>
-    </div>
   );
 }
