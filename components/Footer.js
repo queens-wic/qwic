9d80391069fe59ed39cd9673dfa7b255c6b3a3ff
reneehjk <reneehjkim11@gmail.com>
2024-07-15 19:31:00 -0400

    fixed header and footer no links

--- components/Footer.js
+++ components/Footer.js
@@ -17,35 +17,44 @@ const Footer = () => {
                 <div className="text-2xl font-HeaderFont font-semibold mb-9">Connect with us</div>
                 <div className="grid grid-rows-2 grid-cols-2 gap-8 mb-10">
                     {/* add gap stuff to grid*/}
-                    <div className="flex flex-row gap-2">
+                    <div className="flex flex-row gap-3">
                         <Image src={instagram} alt="Instagram" className='flex flex-row w-[24px] h-[24px]'></Image>
-                        <a href="https://www.instagram.com/queenswic/" aria-label="Instagram" class="text-lg font-HeaderFont text-gray-600 hover:text-gray-800">Instagram</a>
-                    </div>                
-                    <div className="flex flex-row gap-2">
-                        <Image src={tiktok} alt="Tiktok" className='flex flex-row w-[24px] h-[24px]'></Image>
-                        <a href="https://www.tiktok.com/@queenswic" aria-label="TikTok" class="text-lg font-HeaderFont text-gray-600 hover:text-gray-800">TikTok</a>
-                    </div>                
-                    <div className="flex flex-row gap-2">
-                        <Image src={linkedin} alt="LinkedIn" className='flex flex-row w-[24px] h-[24px]'></Image>
-                        <a href="https://ca.linkedin.com/company/queens-qwic" aria-label="LinkedIn" class="text-lg font-HeaderFont text-gray-600 hover:text-gray-800">LinkedIn</a>
-                    </div>                
-                    <div className="flex flex-row gap-2">
+                        <a href="https://www.instagram.com/queenswic/" aria-label="Instagram" className="text-lg font-HeaderFont text-gray-600 hover:text-gray-800">Instagram</a>
+                    </div>  
+                    <div className="flex flex-row gap-3">
                         <Image src={facebook} alt="Facebook" className='flex flex-row w-[24px] h-[24px]'></Image>
-                        <a href="https://www.facebook.com/QueensWIC/" aria-label="Facebook" class="text-lg font-HeaderFont text-gray-600 hover:text-gray-800">Facebook</a>
-                    </div>
+                        <a href="https://www.facebook.com/QueensWIC/" aria-label="Facebook" className="text-lg font-HeaderFont text-gray-600 hover:text-gray-800">Facebook</a>
+                    </div>              
+                    <div className="flex flex-row gap-3">
+                        <Image src={tiktok} alt="Tiktok" className='flex flex-row w-[24px] h-[24px]'></Image>
+                        <a href="https://www.tiktok.com/@queenswic" aria-label="TikTok" className="text-lg font-HeaderFont text-gray-600 hover:text-gray-800">TikTok</a>
+                    </div>                
+                    <div className="flex flex-row gap-3">
+                        <Image src={linkedin} alt="LinkedIn" className='flex flex-row w-[24px] h-[24px]'></Image>
+                        <a href="https://ca.linkedin.com/company/queens-qwic" aria-label="LinkedIn" className="text-lg font-HeaderFont text-gray-600 hover:text-gray-800">LinkedIn</a>
+                    </div>                
                 </div>
             </div>
         </div>
         <div className="h-full w-full">
-            <div className="flex justify-center mt-12 w-full h-[0.1px] border border-[#202632]"></div>
+            <div className="flex justify-center mt-12 mb-10">
+                <div className="w-full h-[1px] bg-[#202632]"></div>
+            </div>
             <div className='flex flex-row justify-between mt-4'>
-                <div className="self-end">© 2024 Queen's Women In Computing</div> 
-                {/* add gap stuff */}
-                <div className='flex flex-row self-end gap-9'>          
-                    <Image src={instagram} href="https://www.instagram.com/queenswic/" alt="Instagram" className='flex flex-row w-[24px] h-[24px]' ></Image>
-                    <Image src={tiktok} href="https://www.tiktok.com/@queenswic" alt="Tiktok" className='flex flex-row w-[24px] h-[24px]'></Image>
-                    <Image src={linkedin} href="https://ca.linkedin.com/company/queens-qwic" alt="LinkedIn" className='flex flex-row w-[24px] h-[24px]'></Image>
-                    <Image src={facebook} href="https://www.facebook.com/QueensWIC/" alt="Facebook" className='flex flex-row w-[24px] h-[24px]'></Image>
+                <div className="self-end text-lg font-HeaderFont">© 2024 Queen's Women In Computing</div> 
+                <div className='flex flex-row self-end gap-9'>
+                    <a href="https://www.instagram.com/queenswic/" target="_blank" rel="noopener noreferrer">
+                        <Image src={instagram} alt="Instagram" className='w-[24px] h-[24px]' />
+                    </a>
+                    <a href="https://www.tiktok.com/@queenswic" target="_blank" rel="noopener noreferrer">
+                        <Image src={tiktok} alt="Tiktok" className='w-[24px] h-[24px]' />
+                    </a>
+                    <a href="https://ca.linkedin.com/company/queens-qwic" target="_blank" rel="noopener noreferrer">
+                        <Image src={linkedin} alt="LinkedIn" className='w-[24px] h-[24px]' />
+                    </a>
+                    <a href="https://www.facebook.com/QueensWIC/" target="_blank" rel="noopener noreferrer">
+                        <Image src={facebook} alt="Facebook" className='w-[24px] h-[24px]' />
+                    </a>
                 </div>
             </div>
             
