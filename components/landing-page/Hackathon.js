6fa2cc54ad5075cde423d13479989bf8f0c75c0f
kale <18kmkf@queensu.ca>
2024-07-19 17:15:33 -0400

    feat: :sparkles: Add hacakthon page bg image

--- components/landing-page/Hackathon.js
+++ components/landing-page/Hackathon.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hackathon = () => {
   return (
-    <div className="h-screen px-36">
+    <div className="h-screen px-36 bg-[url('/landing-page/hackathonbg.svg')]">
       <h1 className="text-[56px] font-semibold">HackHer x Google Hackathon</h1>
       <p className="text-[18px] mt-2">
         At hackHer, we aim to be a welcoming space where individuals of all
