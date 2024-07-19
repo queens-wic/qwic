2cc5c017fdfbf33e985f9747c70fd5f308768f22
kale <18kmkf@queensu.ca>
2024-07-19 17:03:53 -0400

    fix: :art: Reorg components

--- app/page.js
+++ app/page.js
@@ -5,10 +5,6 @@ import WhoAreWe from "@/components/landing-page/WhoAreWe";
 import OurTeam from "@/components/landing-page/OurTeam";
 import Hackathon from "@/components/landing-page/Hackathon";
 import SupportUs from "@/components/landing-page/SupportUs";
-import CardImg from "./HeaderFooter/components/card-img";
-// import CardText from "./HeaderFooter/components/card-text";
-// import Icon from '/Users/kavitathomas/Documents/qwic_dev/qwic/app/HeaderFooter/assets/Vector.png'; //replace this, works for testing purposes
-// import pic from '/Users/kavitathomas/Documents/qwic_dev/qwic/app/HeaderFooter/assets/img_7.png';
 
 export default function Home() {
   return (
