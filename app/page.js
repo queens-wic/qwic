130080098b1e7e12dda4ed3eb6961ad530c606ab
kale <18kmkf@queensu.ca>
2024-07-21 15:11:29 -0400

    Use index.js to clean up imports into one line

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import React from "react";
-// import Header from "./HeaderFooter/components/Header";
-import Hero from "@/components/landing-page/Hero"; // eventually I (kale) will add an index.js to clean up these imports
-import WhoAreWe from "@/components/landing-page/WhoAreWe";
-import OurTeam from "@/components/landing-page/OurTeam";
-import Hackathon from "@/components/landing-page/Hackathon";
-import SupportUs from "@/components/landing-page/SupportUs";
-import LandAck from "@/components/landing-page/LandAck";
+
+import {
+  Hero,
+  WhoAreWe,
+  OurTeam,
+  Hackathon,
+  SupportUs,
+  LandAck,
+} from "..//components/landing-page/";
+
 export default function Home() {
   return (
     <div className="bg-[#F5F5F5]">
---
+++ components/landing-page/index.js
@@ -0,0 +1,8 @@
+// export all landing page components
+export { default as Hackathon } from "./Hackathon";
+export { default as Hero } from "./Hero";
+export { default as LandAck } from "./LandAck";
+export { default as OurTeam } from "./OurTeam";
+export { default as SponserScroll } from "./SponserScroll";
+export { default as SupportUs } from "./SupportUs";
+export { default as WhoAreWe } from "./WhoAreWe";
