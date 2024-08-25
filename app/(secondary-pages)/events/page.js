fa7be8d374b4c976be3f33bd0615227f21a07b6e
kale <18kmkf@queensu.ca>
2024-08-25 09:22:44 -0400

    Initialize events page

--- app/(secondary-pages)/events/page.js
+++ app/(secondary-pages)/events/page.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const page = () => {
-  return <div>Events Page</div>;
+  return (
+    <div>
+      {/* Upcoming Events */}
+      <div className="bg-[url('/mentorship/blobs.png')] bg-cover bg-center h-[250px] flex items-center pt-10">
+        <div className="mx-36">
+          <h1 className="text-[36px] font-boldt">Upcoming Events</h1>
+        </div>
+      </div>
+      <div className="h-full mx-36 flex justify-center items-center py-28">
+        </div>
+    </div>
+  );
 };
 
 export default page;
