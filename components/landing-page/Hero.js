9d6d0246fc4e488e0a87acb181a817de132250e3
kale <18kmkf@queensu.ca>
2024-09-01 10:52:13 -0400

    hero section updates

--- components/landing-page/Hero.js
+++ components/landing-page/Hero.js
@@ -15,9 +15,9 @@ const Hero = () => {
         </div>
         {/* <div className="absolute inset-0 inverted-radius bg-gradient-to-b from-[#DDDBF5]/50 to-[#E0A4D7]/40 z-0"></div> */}
         <div className="relative">
-          <div className="z-10 absolute top-20 -left-44 w-3/6 h-3/6  bg-light-pink rounded-full mix-blend-multiply filter blur-2xl opacity-75 animate-blob"></div>
-          <div className="z-10 absolute bottom-0 -right-4 w-3/6 h-3/6  bg-light-blue rounded-full mix-blend-multiply filter blur-2xl opacity-75 animate-blob animation-delay-2000"></div>
-          <div className="z-10 absolute left-3/4 w-96 h-96  bg-light-purple rounded-full filter mix-blend-multiply blur-2xl opacity-75 animate-blob animation-delay-4000"></div>
+          <div className="z-10 absolute top-20 -left-44 w-2/6 h-2/6  bg-light-pink rounded-full mix-blend-multiply filter blur-2xl opacity-95 animate-blob"></div>
+          {/* <div className="z-10 absolute bottom-0 -right-4 w-3/6 h-3/6  bg-light-blue rounded-full mix-blend-multiply filter blur-2xl opacity-95 animate-blob animation-delay-2000"></div> */}
+          <div className="z-10 absolute left-3/4 w-96 h-96  bg-light-purple rounded-full filter mix-blend-multiply blur-2xl opacity-95 animate-blob animation-delay-4000"></div>
           <div className="relative flex-grow flex flex-col justify-center items-center z-40 mx-8 md:mx-20 lg:mx-36 py-36 md:py-44 sm:py-41">
             <h1 className=" text-6xl font-semibold">
               Helping you discover technology
@@ -29,13 +29,13 @@ const Hero = () => {
             </p>
           </div>
           {/* <div className="bg-[#F5F5F5] w-full h-16 z-10 mt-auto rounded-t-[70px]"></div> */}
-          {/* <Image
+          <Image
             src="/landing-page/hero.png"
             // className="align-center"
             width={1920}
             height={900}
             alt="Digital illustration of a girl smiling and making an OK symbol with her left hand."
-          /> */}
+          />
         </div>
       </div>
     </div>
