0d567349b5ba467dcf661146f491c2b25a6eb1f1
kale <18kmkf@queensu.ca>
2024-09-01 11:27:27 -0400

    add animation for mentorship page header

--- app/(secondary-pages)/mentorship/page.js
+++ app/(secondary-pages)/mentorship/page.js
@@ -5,11 +5,13 @@ const page = () => {
   return (
     <div>
       {/* Heading */}
-      <div className="bg-[url('/mentorship/blobs.png')] bg-cover bg-center h-[250px] flex justify-center items-center pt-10">
-        <div className="mx-36">
+      <div className="flex justify-center items-center pt-10 mx-36">
+        <div className="relative">
           <h1 className="text-[36px] font-bold">
             Bits and Bytes Mentorship Program
           </h1>
+          <div className="z-10 absolute top-0 -left-10 w-44 h-44  bg-light-pink rounded-full mix-blend-multiply filter blur-2xl opacity-95 animate-blob"></div>
+          <div className="z-10 absolute right-10 w-44 h-44  bg-light-purple rounded-full filter mix-blend-multiply blur-2xl opacity-95 animate-blob animation-delay-4000"></div>
           <p className="text-secondary-text text-[18px] mt-4">
             QWIC's Bits and Bytes peer mentorship program is a chance for you to
             be paired with an upper year student in Computing. Whether you need
