397d4b90ea651b786e464396f57211043995395c
kale <18kmkf@queensu.ca>
2024-09-01 11:05:59 -0400

    our team page styling

--- components/landing-page/OurTeam.js
+++ components/landing-page/OurTeam.js
@@ -1,29 +1,37 @@
 import React from "react";
 import Image from "next/image";
-
+import Link from "next/link";
 const OurTeam = () => {
   return (
-    <div className="h-screen mx-36 flex flex-col justify-center">
+    <div className="h-screen mx-8 md:mx-20 lg:mx-36 flex flex-col justify-center">
       <h1 className="text-[36px] font-bold">
         Creating Connections and Learning Together
       </h1>
       <p className="text-[18px] mt-4 text-secondary-text">
         QWIC is a community for all women, underrepresented minorities, and
-        anyone who is <br /> passionate about technology and share the same
-        values of equality.
+        anyone who is passionate about technology and share the same values of
+        equality.
       </p>
       <div className="self-center p-4">
         <Image
-          src="/landing-page/OurTeamImage.png"
-          width={1000}
+          src="/landing-page/ourTeam.png"
+          width={1500}
           height={900}
           alt="Collage of QWIC team pictures"
         />
       </div>
       <div className="flex justify-end">
-        <button className="bg-qwic-pink transition ease-in-out delay-100 hover:translate-x-1 hover:bg-light-purple rounded-full border border-black py-2 px-4 font-semibold">
-          Meet Our Team
-        </button>
+        <Link href="/our-team">
+          <div className="flex justify-between items-center gap-6 w-full px-6 py-6 transition ease-in-out delay-100 hover:translate-x-2">
+            <span className="text-[20px] font-semi-bold">Meet our team</span>
+            <Image
+              src={`/assets/arrow.svg`}
+              alt="Learn More"
+              width={25}
+              height={25}
+            />
+          </div>
+        </Link>
       </div>
     </div>
   );
