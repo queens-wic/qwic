7d5e8cc75320c49c53a4d0b93d490e5d34b513af
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2024-09-03 17:18:15 -0400

    data and pics for team updated

--- app/our-team/members.js
+++ app/our-team/members.js
@@ -1,53 +1,52 @@
-import React from "react"
+import React from "react";
 
-import allix from "@/our-team/profile-pics/allix.png"
-import amanda from "@/our-team/profile-pics/amanda.jpg"
-import ashley from "@/our-team/profile-pics/Ashley.jpg"
-import beth from "@/our-team/profile-pics/beth.png"
-import emmalb from "@/our-team/profile-pics/emma.jpeg"
-import emmah from "@/our-team/profile-pics/emmaH.jpg"
-import jade from "@/our-team/profile-pics/jade.png"
-import kate from "@/our-team/profile-pics/kate.jpg"
-import kavita from "@/our-team/profile-pics/kavita.jpg"
-import lauren from "@/our-team/profile-pics/lauren.JPG"
-import lia from "@/our-team/profile-pics/lia.png"
-import lilyana from "@/our-team/profile-pics/lilyana.png"
-import madeleine from "@/our-team/profile-pics/Madeleine.jpeg"
-import maria from "@/our-team/profile-pics/maria.jpeg"
-import navya from "@/our-team/profile-pics/navya.jpg"
-import pheobe from "@/our-team/profile-pics/pheobe.png"
-import renee from "@/our-team/profile-pics/renee.png"
-import saanvi from "@/our-team/profile-pics/saanvi.JPG"
-import sari from "@/our-team/profile-pics/sari.png"
-import teagan from "@/our-team/profile-pics/teagan.png"
-
-import emptyprofile from "@/public/assets/emptyprofile.png" //use as temp filler photo
+import allix from "qwic/public/profile-pics/allix.png";
+import amanda from "qwic/public/profile-pics/amanda.jpg"
+import ashley from "qwic/public/profile-pics/Ashley.jpg"
+import beth from "qwic/public/profile-pics/beth.png"
+import emmalb from "qwic/public/profile-pics/emma.jpeg"
+import jade from "qwic/public/profile-pics/jade.png"
+import kate from "qwic/public/profile-pics/kate.jpg"
+import kavita from "qwic/public/profile-pics/kavita.jpg"
+import lauren from "qwic/public/profile-pics/lauren.JPG"
+import lia from "qwic/public/profile-pics/lia.png"
+import lilyana from "qwic/public/profile-pics/lilyana.png"
+import madeleine from "qwic/public/profile-pics/Madeleine.jpeg"
+import maria from "qwic/public/profile-pics/maria.jpeg"
+import navya from "qwic/public/profile-pics/navya.jpg"
+import pheobe from "qwic/public/profile-pics/pheobe.png"
+import renee from "qwic/public/profile-pics/renee.png"
+import saanvi from "qwic/public/profile-pics/saanvi.JPG"
+import sari from "qwic/public/profile-pics/sari.png"
+import teagan from "qwic/public/profile-pics/teagan.png"
+import emptyprofile from "qwic/public/profile-pics/emptyprofile.png" //use as temp filler photo
 
 // use place photo for Annika Tran, Keely McSpurren, Alyssa Zhao, Rena Hajjar, Kaleigh Feder
 
 const profiles = [
-  {src: {jade}, alt: 'Jade Warren, Co-Chair', name: 'Jade Warren', role: 'Co-Chair'},
   {src: {maria}, alt: 'Maria Kobilaski, Co-Chair', name: 'Maria Kobilaski', role: 'Co-Chair'},
-  {src: {allix}, alt: 'Allix Fletcher, Finance', name: 'Allix Fletcher', role: 'Finance'},
-  {src: {amanda}, alt: 'Amanda Li, Graphic Designer', name: 'Amanda Li', role: 'Graphic Designer'},
-  {src: {ashley}, alt: 'Ashley Van Rooyen, Community Coordinator', name: 'Ashley Van Rooyen', role: 'Community Coordinator'},
+  {src: {jade}, alt: 'Jade Warren, Co-Chair', name: 'Jade Warren', role: 'Co-Chair'},
+  {src: {pheobe}, alt: 'Phoebe Starnio, Vice-Chair of Community', name: 'Phoebe Starnio', role: 'Vice-Chair of Community'},
   {src: {navya}, alt: 'Navya Bajwa, Community Coordinator', name: 'Navya Bajwa', role: 'Community Coordinator'},
   {src: {lilyana}, alt: 'Lilyana Boraniev, Community Coordinator', name: 'Lilyana Boraniev', role: 'Community Coordinator'},
-  {src: {beth}, alt: 'Beth Armstrong, Mentorship Director', name: 'Beth Armstrong', role: 'Mentorship Director'},
-  {src: {madeleine}, alt: 'Madeleine Domantay, Mentorship Coordinator', name: 'Madeleine Domantay', role: 'Mentorship Coordinator'},
-  {src: {kate}, alt: 'Kate Ram, Mentorship Coordinator', name: 'Kate Ram', role: 'Mentorship Coordinator'},
-  {src: {lauren}, alt: 'Lauren Lidhar, Mentorship Coordinator', name: 'Lauren Lidhar', role: 'Mentorship Coordinator'},
+  {src: {ashley}, alt: 'Ashley Van Rooyen, Community Coordinator', name: 'Ashley Van Rooyen', role: 'Community Coordinator'},
+  {src: {renee}, alt: 'Renee Kim, Vice-Chair of Marketin', name: 'Renee Kim', role: 'Vice-Chair of Marketing'},
   {src: {emmalb}, alt: 'Emma Leitch-Blais, Marketing Coordinator', name: 'Emma Leitch-Blais', role: 'Marketing Coordinator'},
   {src: {emptyprofile}, alt: 'Alyssa Zhao, Marketing Coordinator', name: 'Alyssa Zhao', role: 'Marketing Coordinator'},
+  {src: {amanda}, alt: 'Amanda Li, Graphic Designer', name: 'Amanda Li', role: 'Graphic Designer'},
   {src: {emptyprofile}, alt: 'Kaleigh Feder, Web Developer', name: 'Kaleigh Feder', role: 'Web Developer'},
   {src: {kavita}, alt: 'Kavita Thomas, Web Developer', name: 'Kavita Thomas', role: 'Web Developer'},
-  {src: {lia}, alt: 'Lia Silver, Operations', name: 'Lia Silver', role: 'Operations'},
-  {src: {pheobe}, alt: 'Phoebe Starnio, Community Director', name: 'Phoebe Starnio', role: 'Community Director'},
-  {src: {renee}, alt: 'Renee Kim, Marketing Director', name: 'Renee Kim', role: 'Marketing Director'},
-  {src: {saanvi}, alt: 'Saanvi Bakshi, HackHer Coordinator', name: 'Saanvi Bakshi', role: 'HackHer Coordinator'},
-  {src: {sari}, alt: 'Sari Pagurek Van Mossal, HackHer Coordinator', name: 'Sari Pagurek Van Mossal', role: 'HackHer Coordinator'},
-  {src: {teagan}, alt: 'Teagan Wright, HackHer Coordinator', name: 'Teagan Wright', role: 'HackHer Coordinator'},
-  {src: {emptyprofile}, alt: 'Annika Tran, Sponsorships Coordinator', name: 'Annika Tran', role: 'Sponsorships Coordinator'},
+  {src: {lia}, alt: 'Lia Silver, Vice-Chair of Operations', name: 'Lia Silver', role: 'Vice-Chair of Operations'},
   {src: {emptyprofile}, alt: 'Keely McSpurren, Senior Advisor', name: 'Keely McSpurren', role: 'Senior Advisor'},
+  {src: {emptyprofile}, alt: 'Annika Tran, Sponsorships Coordinator', name: 'Annika Tran', role: 'Sponsorships Coordinator'},
+  {src: {allix}, alt: 'Allix Fletcher, Finance Coordinator', name: 'Allix Fletcher', role: 'Finance Coordinator'},
+  {src: {sari}, alt: 'Sari Pagurek Van Mossal, Vice-Chair of HackHer', name: 'Sari Pagurek Van Mossal', role: 'Vice-Chair of HackHer'},
+  {src: {saanvi}, alt: 'Saanvi Bakshi, HackHer Coordinator', name: 'Saanvi Bakshi', role: 'HackHer Coordinator'},
   {src: {emptyprofile}, alt: 'Rena Hajjar, HackHer Coordinator', name: 'Rena Hajjar', role: 'HackHer Coordinator'},
-]
+  {src: {teagan}, alt: 'Teagan Wright, HackHer Coordinator', name: 'Teagan Wright', role: 'HackHer Coordinator'},
+  {src: {beth}, alt: 'Beth Armstrong, Vice-Chair of Mentorship', name: 'Beth Armstrong', role: 'Vice-Chair of Mentorship'},
+  {src: {madeleine}, alt: 'Madeleine Domantay, Mentorship Coordinator', name: 'Madeleine Domantay', role: 'Mentorship Coordinator'},
+  {src: {kate}, alt: 'Kate Ram, Mentorship Coordinator', name: 'Kate Ram', role: 'Mentorship Coordinator'},
+  {src: {lauren}, alt: 'Lauren Lidhar, Mentorship Coordinator', name: 'Lauren Lidhar', role: 'Mentorship Coordinator'}
+];
+
--- app/our-team/page.js
+++ app/our-team/page.js
@@ -5,137 +5,41 @@ import CardImgBlue from "@/components/cards/card-img-blue";
 import CardImgPink from "@/components/cards/card-img-pink";
 import CardImgPurple from "@/components/cards/card-img-purple";
 import MemberProfile from "./member-profile";
-import gitpub from "@/public/assets/gitpub.jpg"
+import gitpub from "@/public/assets/gitpub.jpg";
 
+import profiles from "./members";
 
 const Page = () => {
+  const cardColors = [CardImgPink, CardImgBlue, CardImgPurple];
   return (
     <div className='h-screen'>
-
-      <div className="p-4"/>
-      <Header/>
+      <div className="p-4" />
+      <Header />
 
       <div className="mx-36">
-
         <h1 className="text-[40px] font-bold pt-16 px-0">Meet our Team</h1>
-        <p className="pt-10 pb-16 px-0">QWIC is a community for all women, underrepresented minorities, and anyone who is passionate about technology and share the same values of equality.</p>
+        <p className="pt-10 pb-16 px-0">
+          QWIC is a community for all women, underrepresented minorities, and anyone who is passionate about technology and share the same values of equality.
+        </p>
 
         <div className="flex flex-wrap justify-between justify-center px-0">
-          <div>
-            <MemberProfile
-              card={<CardImgPink img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgBlue img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgPurple img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgPink img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgBlue img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgPurple img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgPink img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgBlue img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgPurple img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgPink img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgBlue img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgPurple img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgPink img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgBlue img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-          <div>
-            <MemberProfile
-              card={<CardImgPurple img={gitpub}/>}
-              name={'Name'}
-              role={'role'}
-            />
-          </div>
-
+          {profiles.map((profile, index) => (
+            <div key={index}>
+              <MemberProfile
+                card={<CardImgPink img={gitpub}/>}
+                name={profile.name}
+                role={profile.role}
+              />
+            </div>
+          ))}
         </div>
       </div>
 
       <div className="h-40"></div>
-      <Footer/>
-
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Page;
 
