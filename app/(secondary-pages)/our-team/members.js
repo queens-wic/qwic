6e5a81d1467297d35d6cf044fd55ef6c9d3ce061
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2024-09-07 14:40:36 -0400

    profile pics path updated

--- app/(secondary-pages)/our-team/members.js
+++ app/(secondary-pages)/our-team/members.js
@@ -1,52 +1,30 @@
-
-import allix from "@/public/profile-pics/allix.jpg";
-import amanda from "@/public/profile-pics/amanda.jpg"
-import ashley from "@/public/profile-pics/Ashley.jpg"
-import beth from "@/public/profile-pics/beth.jpg"
-import emmalb from "@/public/profile-pics/emma.jpg"
-import jade from "@/public/profile-pics/jade.jpg"
-import kate from "@/public/profile-pics/kate.jpg"
-import kavita from "@/public/profile-pics/kavita.jpg"
-import lauren from "@/public/profile-pics/lauren.jpg"
-import lia from "@/public/profile-pics/lia.jpg"
-import lilyana from "@/public/profile-pics/lilyana.jpg"
-import madeleine from "@/public/profile-pics/Madeleine.jpg"
-import maria from "@/public/profile-pics/maria.jpg"
-import navya from "@/public/profile-pics/navya.jpg"
-import pheobe from "@/public/profile-pics/pheobe.jpg"
-import renee from "@/public/profile-pics/renee.jpg"
-import saanvi from "@/public/profile-pics/saanvi.jpg"
-import sari from "@/public/profile-pics/sari.jpg"
-import teagan from "@/public/profile-pics/teagan.jpg"
-import emptyprofile from "@/public/profile-pics/emptyprofile.jpg" //use as temp filler photo
-
 // use place photo for Annika Tran, Keely McSpurren, Alyssa Zhao, Rena Hajjar, Kaleigh Feder
 
 const profiles = [
-  {src: {maria}, alt: 'Maria Kobilaski, Co-Chair', name: 'Maria Kobilaski', role: 'Co-Chair'},
-  {src: {jade}, alt: 'Jade Warren, Co-Chair', name: 'Jade Warren', role: 'Co-Chair'},
-  {src: {pheobe}, alt: 'Phoebe Starnio, Vice-Chair of Community', name: 'Phoebe Starnio', role: 'Vice-Chair of Community'},
-  {src: {navya}, alt: 'Navya Bajwa, Community Coordinator', name: 'Navya Bajwa', role: 'Community Coordinator'},
-  {src: {lilyana}, alt: 'Lilyana Boraniev, Community Coordinator', name: 'Lilyana Boraniev', role: 'Community Coordinator'},
-  {src: {ashley}, alt: 'Ashley Van Rooyen, Community Coordinator', name: 'Ashley Van Rooyen', role: 'Community Coordinator'},
-  {src: {renee}, alt: 'Renee Kim, Vice-Chair of Marketing', name: 'Renee Kim', role: 'Vice-Chair of Marketing'},
-  {src: {emmalb}, alt: 'Emma Leitch-Blais, Marketing Coordinator', name: 'Emma Leitch-Blais', role: 'Marketing Coordinator'},
-  {src: {emptyprofile}, alt: 'Alyssa Zhao, Marketing Coordinator', name: 'Alyssa Zhao', role: 'Marketing Coordinator'},
-  {src: {amanda}, alt: 'Amanda Li, Graphic Designer', name: 'Amanda Li', role: 'Graphic Designer'},
-  {src: {emptyprofile}, alt: 'Kaleigh Feder, Web Developer', name: 'Kaleigh Feder', role: 'Web Developer'},
-  {src: {kavita}, alt: 'Kavita Thomas, Web Developer', name: 'Kavita Thomas', role: 'Web Developer'},
-  {src: {lia}, alt: 'Lia Silver, Vice-Chair of Operations', name: 'Lia Silver', role: 'Vice-Chair of Operations'},
-  {src: {emptyprofile}, alt: 'Keely McSpurren, Senior Advisor', name: 'Keely McSpurren', role: 'Senior Advisor'},
-  {src: {emptyprofile}, alt: 'Annika Tran, Sponsorships Coordinator', name: 'Annika Tran', role: 'Sponsorships Coordinator'},
-  {src: {allix}, alt: 'Allix Fletcher, Finance Coordinator', name: 'Allix Fletcher', role: 'Finance Coordinator'},
-  {src: {sari}, alt: 'Sari Pagurek Van Mossal, Vice-Chair of HackHer', name: 'Sari Pagurek Van Mossal', role: 'Vice-Chair of HackHer'},
-  {src: {saanvi}, alt: 'Saanvi Bakshi, HackHer Coordinator', name: 'Saanvi Bakshi', role: 'HackHer Coordinator'},
-  {src: {emptyprofile}, alt: 'Rena Hajjar, HackHer Coordinator', name: 'Rena Hajjar', role: 'HackHer Coordinator'},
-  {src: {teagan}, alt: 'Teagan Wright, HackHer Coordinator', name: 'Teagan Wright', role: 'HackHer Coordinator'},
-  {src: {beth}, alt: 'Beth Armstrong, Vice-Chair of Mentorship', name: 'Beth Armstrong', role: 'Vice-Chair of Mentorship'},
-  {src: {madeleine}, alt: 'Madeleine Domantay, Mentorship Coordinator', name: 'Madeleine Domantay', role: 'Mentorship Coordinator'},
-  {src: {kate}, alt: 'Kate Ram, Mentorship Coordinator', name: 'Kate Ram', role: 'Mentorship Coordinator'},
-  {src: {lauren}, alt: 'Lauren Lidhar, Mentorship Coordinator', name: 'Lauren Lidhar', role: 'Mentorship Coordinator'}
+  {src: '/profile-pics/maria.jpg', alt: 'Maria Kobilaski, Co-Chair', name: 'Maria Kobilaski', role: 'Co-Chair'},
+  {src: '/profile-pics/jade.jpg', alt: 'Jade Warren, Co-Chair', name: 'Jade Warren', role: 'Co-Chair'},
+  {src: '/profile-pics/pheobe.jpg', alt: 'Phoebe Starnio, Vice-Chair of Community', name: 'Phoebe Starnio', role: 'Vice-Chair of Community'},
+  {src: '/profile-pics/navya.jpg', alt: 'Navya Bajwa, Community Coordinator', name: 'Navya Bajwa', role: 'Community Coordinator'},
+  {src: '/profile-pics/lilyana.jpg', alt: 'Lilyana Boraniev, Community Coordinator', name: 'Lilyana Boraniev', role: 'Community Coordinator'},
+  {src: '/profile-pics/Ashley.jpg', alt: 'Ashley Van Rooyen, Community Coordinator', name: 'Ashley Van Rooyen', role: 'Community Coordinator'},
+  {src: '/profile-pics/renee.jpg', alt: 'Renee Kim, Vice-Chair of Marketing', name: 'Renee Kim', role: 'Vice-Chair of Marketing'},
+  {src: '/profile-pics/emma.jpg', alt: 'Emma Leitch-Blais, Marketing Coordinator', name: 'Emma Leitch-Blais', role: 'Marketing Coordinator'},
+  {src: '/profile-pics/emptyprofile.jpg', alt: 'Alyssa Zhao, Marketing Coordinator', name: 'Alyssa Zhao', role: 'Marketing Coordinator'},
+  {src: '/profile-pics/amanda.jpg', alt: 'Amanda Li, Graphic Designer', name: 'Amanda Li', role: 'Graphic Designer'},
+  {src: '/profile-pics/emptyprofile.jpg', alt: 'Kaleigh Feder, Web Developer', name: 'Kaleigh Feder', role: 'Web Developer'},
+  {src: '/profile-pics/kavita.jpg', alt: 'Kavita Thomas, Web Developer', name: 'Kavita Thomas', role: 'Web Developer'},
+  {src: '/profile-pics/lia.jpg', alt: 'Lia Silver, Vice-Chair of Operations', name: 'Lia Silver', role: 'Vice-Chair of Operations'},
+  {src: '/profile-pics/emptyprofile.jpg', alt: 'Keely McSpurren, Senior Advisor', name: 'Keely McSpurren', role: 'Senior Advisor'},
+  {src: '/profile-pics/emptyprofile.jpg', alt: 'Annika Tran, Sponsorships Coordinator', name: 'Annika Tran', role: 'Sponsorships Coordinator'},
+  {src: '/profile-pics/allix.jpg', alt: 'Allix Fletcher, Finance Coordinator', name: 'Allix Fletcher', role: 'Finance Coordinator'},
+  {src: '/profile-pics/sari.jpg', alt: 'Sari Pagurek Van Mossal, Vice-Chair of HackHer', name: 'Sari Pagurek Van Mossal', role: 'Vice-Chair of HackHer'},
+  {src: '/profile-pics/saanvi.jpg', alt: 'Saanvi Bakshi, HackHer Coordinator', name: 'Saanvi Bakshi', role: 'HackHer Coordinator'},
+  {src: '/profile-pics/emptyprofile.jpg', alt: 'Rena Hajjar, HackHer Coordinator', name: 'Rena Hajjar', role: 'HackHer Coordinator'},
+  {src: '/profile-pics/teagan.jpg', alt: 'Teagan Wright, HackHer Coordinator', name: 'Teagan Wright', role: 'HackHer Coordinator'},
+  {src: '/profile-pics/beth.jpg', alt: 'Beth Armstrong, Vice-Chair of Mentorship', name: 'Beth Armstrong', role: 'Vice-Chair of Mentorship'},
+  {src: '/profile-pics/Madeleine.jpg', alt: 'Madeleine Domantay, Mentorship Coordinator', name: 'Madeleine Domantay', role: 'Mentorship Coordinator'},
+  {src: '/profile-pics/kate.jpg', alt: 'Kate Ram, Mentorship Coordinator', name: 'Kate Ram', role: 'Mentorship Coordinator'},
+  {src: '/profile-pics/lauren.jpg', alt: 'Lauren Lidhar, Mentorship Coordinator', name: 'Lauren Lidhar', role: 'Mentorship Coordinator'}
 ];
 
 export default profiles;
