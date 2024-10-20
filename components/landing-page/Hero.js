bdc36d07edadcc09d5ceac1c66508cb5af3e1874
kale <18kmkf@queensu.ca>
2024-10-20 12:18:50 -0400

    Updates based on feedback

--- components/landing-page/Hero.js
+++ components/landing-page/Hero.js
@@ -10,16 +10,10 @@ const Hero = () => {
           <Navbar />
         </div>
         <div className="relative">
-          {/* <div className="z-10 absolute top-20 -left-44 w-2/6 h-2/6  bg-light-purple rounded-full mix-blend-multiply filter blur-2xl opacity-95 animate-blob"></div>
-          <div className="z-10 absolute left-3/4 w-96 h-96  bg-light-pink rounded-full filter mix-blend-multiply blur-2xl opacity-95 animate-blob animation-delay-4000"></div> */}
+          <div className="z-10 absolute top-20 -left-44 w-2/6 h-2/6  bg-light-purple rounded-full mix-blend-multiply filter blur-2xl opacity-95 animate-blob"></div>
+          <div className="z-10 absolute left-3/4 w-96 h-96  bg-light-pink rounded-full filter mix-blend-multiply blur-2xl opacity-95 animate-blob animation-delay-4000"></div>
           <div className="relative flex-grow flex flex-col justify-center items-center z-40 mx-4 sm:mx-8 md:mx-16 lg:mx-36 py-36 md:py-44 sm:py-41">
             <div className="flex sm:flex-row flex-col gap-2 mb-2 items-center z-0">
-              <Image
-                src="/landing-page/logo2.png"
-                width={100}
-                height={100}
-                alt="Hero logo"
-              />
               <h1 className="text-4xl sm:text-5xl lg:text-6xl font-semibold text-center">
                 Queen's Women in Computing
               </h1>
